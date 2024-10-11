a67537eb3d2264e9e0e0c689a6575ccd69cb7840
joesmy <jwsmy05@gmail.com>
2024-10-11 15:35:58 +0100

    testing floats

--- IntegerDoublingFunctionUnitTest.py
+++ IntegerDoublingFunctionUnitTest.py
@@ -12,6 +12,10 @@ class TestNum(unittest.TestCase):
         result = twiceNumber(17)
         self.assertEqual(result,34)
 
+    def testFloat(self):
+        result = twiceNumber(5.5)
+        self.assertFalse(result,)
+
 if __name__ == '__main__':
     unittest.main()
         
